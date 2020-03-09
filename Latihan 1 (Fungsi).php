9c598737ff7f3f2d78201fd828728150503df6cf
lethanfadlil <fadlillah_bashir_28rpl@student.smktelkom-mlg.sch.id>
2020-03-09 09:52:51 +0700

    first commit

---
+++ Latihan 1 (Fungsi).php
@@ -0,0 +1,19 @@
+<?php
+
+    // membuat fungsi
+    function perkenalan() {
+        echo "Assalamu'alaikum, ";
+        echo "Perkenalkan, nama saya Ardi<br/>";
+        echo "Senang berkenalan denganmu<br/>";
+    }
+
+    // memanggil fungsi yang sudah dibuat
+
+    perkenalan();
+
+    echo "<hr>";
+
+    // memanggilnya
+
+    perkenalan();
+?>
---
+++ Latihan 2 dan 3.php
@@ -0,0 +1,21 @@
+<?php
+    function luas_lingkaran ($jari) {
+        return 3.14 * $jari * $jari;
+    }
+    //pemanggilan fungsi
+
+    $r = 10;
+    echo "Luas lingkaran dengan jari-jari $r = ";
+    echo luas_lingkaran($r);
+?>
+
+<?php
+    function head() {
+        echo '
+        <link rel="stylesheet" type="text/css" href="style/style.css">
+        <link rel="stylesheet" type="text/css" href="style/font-awesome.css"> ';
+    }
+    //pemanggilan fungsi
+
+    echo luas_lingkaran($r);
+?>
